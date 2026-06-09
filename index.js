c6431f5c0814f493b61d61cdaf5c0ba03f1fee23
abdullah <hk7485966@gmail.com>
2026-06-09 19:43:35 +0500

    chore: finalize server setup and routes integration

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+// const express = require("express");
+// const cors = require("cors");
+// require("dotenv").config();
+
+// const app = express();
+// const connectDB = require("./config/db");
+// connectDB();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.json({ message: "Todo Server is running" });
+// });
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const connectDB = require("./config/db");
+const todoRoutes = require("./routes/todoRoutes");
 
 const app = express();
-const connectDB = require("./config/db");
+
 connectDB();
 
 app.use(cors());
@@ -13,8 +39,12 @@ app.get("/", (req, res) => {
   res.json({ message: "Todo Server is running" });
 });
 
+app.use("/api/todos", todoRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
