2070b51d247197fc3e8f775468325f96fdac7c80
abdullah <hk7485966@gmail.com>
2026-06-10 09:38:08 +0500

    feat: implement full CRUD todo API with status update support

--- models/Todo.js
+++ models/Todo.js
@@ -1,3 +1,27 @@
+// const mongoose = require("mongoose");
+
+// const todoSchema = new mongoose.Schema(
+//   {
+//     text: {
+//       type: String,
+//       required: true,
+//     },
+//     completed: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Todo", todoSchema);
+
+
+
+
+
+
+
 const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema(
@@ -5,13 +29,16 @@ const todoSchema = new mongoose.Schema(
     text: {
       type: String,
       required: true,
+      trim: true,
     },
     completed: {
       type: Boolean,
       default: false,
     },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Todo", todoSchema);
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,41 +1,176 @@
+// const express = require("express");
+// const router = express.Router();
+// const Todo = require("../models/Todo");
+// const todoRoutes = require("./routes/todoRoutes");
+
+
+// app.use("/api/todos", todoRoutes);
+
+// // GET all todos
+// router.get("/", async (req, res) => {
+//   const todos = await Todo.find().sort({ createdAt: -1 });
+//   res.json(todos);
+// });
+
+// // CREATE todo
+// router.post("/", async (req, res) => {
+//   const todo = new Todo({
+//     text: req.body.text,
+//   });
+
+//   await todo.save();
+//   res.json(todo);
+// });
+
+// // DELETE todo
+// router.delete("/:id", async (req, res) => {
+//   await Todo.findByIdAndDelete(req.params.id);
+//   res.json({ message: "Todo deleted" });
+// });
+
+// // UPDATE todo (toggle complete)
+// router.put("/:id", async (req, res) => {
+//   const todo = await Todo.findById(req.params.id);
+//   todo.completed = !todo.completed;
+//   await todo.save();
+//   res.json(todo);
+// });
+
+
+
+
+
+
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
-const todoRoutes = require("./routes/todoRoutes");
-
-
-app.use("/api/todos", todoRoutes);
 
 // GET all todos
 router.get("/", async (req, res) => {
-  const todos = await Todo.find().sort({ createdAt: -1 });
-  res.json(todos);
+  try {
+    const todos = await Todo.find().sort({
+      createdAt: -1,
+    });
+
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
+
+
 
 // CREATE todo
 router.post("/", async (req, res) => {
-  const todo = new Todo({
-    text: req.body.text,
-  });
+  try {
+    const { text, completed } = req.body;
 
-  await todo.save();
-  res.json(todo);
+    const todo = await Todo.create({
+      text,
+      completed,
+    });
+
+    res.status(201).json(todo);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
+
 
 // DELETE todo
 router.delete("/:id", async (req, res) => {
-  await Todo.findByIdAndDelete(req.params.id);
-  res.json({ message: "Todo deleted" });
+  try {
+    const todo = await Todo.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!todo) {
+      return res.status(404).json({
+        message: "Todo not found",
+      });
+    }
+
+    res.json({
+      message: "Todo deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-// UPDATE todo (toggle complete)
+
+// TOGGLE todo
 router.put("/:id", async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  todo.completed = !todo.completed;
-  await todo.save();
-  res.json(todo);
+  try {
+    const { text, completed } = req.body;
+
+    const todo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      {
+        text,
+        completed,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!todo) {
+      return res.status(404).json({
+        message: "Todo not found",
+      });
+    }
+
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
+
+
+
+router.patch("/:id/status", async (req, res) => {
+  try {
+    const { completed } = req.body;
+
+    const todo = await Todo.findByIdAndUpdate(
+      req.params.id,
+      {
+        completed,
+      },
+      {
+        new: true,
+      }
+    );
+
+    res.json(todo);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 
 
@@ -43,3 +178,5 @@ router.put("/:id", async (req, res) => {
 
 
 module.exports = router;
+
+
